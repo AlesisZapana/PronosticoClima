6061db26521a2cc6a513fdb8732b27cf3520141a
Alesis <ale9410@gmail.com>
2022-11-05 10:46:53 -0300

    Agregado íconos

--- js/index.js
+++ js/index.js
@@ -2,52 +2,58 @@ var fecha=' '
 apiClima='https://api.open-meteo.com/v1/forecast?latitude=-54.8019&longitude=-68.303&hourly=temperature_2m,apparent_temperature,precipitation,showers,windspeed_10m&timezone=America%2FSao_Paulo'
 
 dayjs.locale('es')
-
-const evaluaLluvia=(lluvia)=>{
-    if (lluvia<2) {
-        return `Lluvias leves`
+// Si es el día de la fecha, utiliza un formato animado. De lo contrario usa un estático.
+// en clima utiliza el texto del clima pronosticado
+const iconoClima=(hora,clima)=>{
+    let tipoImagen=dayjs(hora).format('YYMMDD')==dayjs().format('YYMMDD') ? 'animated' :'static'
+    let diaNoche=(parseInt(dayjs(hora).format('HH'))>=20|| parseInt(dayjs(hora).format('HH'))<=6)? '-night' : '-day'
+    return `<img src="resources/${tipoImagen}/${clima}${diaNoche}.svg">`
+}
+const evaluaLluvia=(lluvia,hora)=>{
+    if (lluvia>0 && lluvia<2) {
+        return `${iconoClima(hora,'rainy-1')} <p>Lluvias leves</p>`
     }else if (lluvia>=2 && lluvia<15){
-        return `Lluvia`
+        return `${iconoClima(hora,'rainy-2')} <p>Lluvia</p>`
     }else if (lluvia>=15 && lluvia<30 ) {
-        return `Lluvias fuertes`
+        return `${iconoClima(hora,'rainy-3')} <p>Lluvias fuertes</p>`
     }else if(lluvia>=30 && lluvia<60){
-        return `Lluvias muy fuertes`
+        return `${iconoClima(hora,'rainy-3')} <p>Lluvias muy fuertes</p>`
+    }else if (lluvia>=60){
+        return `${iconoClima(hora,'scattered-thunderstorms')} <p>Lluvias torrenciales</p>`
     }else{
-        return `Lluvias torrenciales`
+        return `${iconoClima(hora,'clear')} <p>Sin lluvias</p>`
     }
 }
 
 const tarjeta =(temperatura, tiempo, sensacionTermica,precipitacion, lluvia, viento, unidades)=>{
     // inicializa variable
-    let html_tarjeta=` `
-
+    let htmlTarjeta=` `
     // Si el día (sin la hora), es distinto al que se recibe por parámetro () entonces se crea un encabezado para la fecha
     if ( dayjs(tiempo).format('YYYYMMDD')!=dayjs(fecha).format('YYYYMMDD') ){
         fecha=tiempo
         let fechaFormateada=dayjs(tiempo).format('dddd DD')+' de '+dayjs(tiempo).format('MMMM')
-        html_tarjeta+=`<div class="col-sm-12"><h2>${fechaFormateada}</h2></div>`
+        htmlTarjeta+=`<div class="col-sm-12"><h2>${fechaFormateada}</h2></div>`
     }
 
     // estilos para la tarjeta
-    html_tarjeta+= `
+    htmlTarjeta+= `
     <div class="col-sm-4 col-lg-2 card">
         <div class="card-body">
             <h5 class="card-title">${dayjs(tiempo).format('HH:mm')}</h5>
+            <h6>${evaluaLluvia(lluvia,tiempo)}</h6>
             <h6>Temperatura: ${temperatura}${unidades.temperature_2m}</h6> 
             <h6>Sens. térmica: ${sensacionTermica}${unidades.apparent_temperature} </h6>
-            <h6>${evaluaLluvia(lluvia) }</h6>
             <h6>Viento: ${viento}${unidades.windspeed_10m} </h6>
         </div>
     </div>
     `
-                // <h6>Precipitaciones: ${precipitacion}${unidades.precipitation}</h6>
-                // <h6>Lluvias: ${lluvia}${unidades.showers}</h6>
-    return html_tarjeta
+    // <h6>Precipitaciones: ${precipitacion}${unidades.precipitation}</h6>
+    // <h6>Lluvias: ${lluvia}${unidades.showers}</h6>
+    return htmlTarjeta
 }
 
 const generaPlantilla= (datosHora, unidades)=>{
     /*
-      Se inicializa el arreglo con [] (en la línea 61 ).
       Cada vez que la hora indicada coincida con la hora del elemento, se introduce la posición (index) en el arreglo.
       Al finalizar se retorna el arreglo con los índices.
     */
